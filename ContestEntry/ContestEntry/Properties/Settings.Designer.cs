764260ddb3c83e260156426e29ae44e5633c3360
meowFlute <mscottchristensen@gmail.com>
2016-04-20 21:03:14 -0600

    moved the database into it's own folder and added two scripts representing the creation of the tables

--- ContestEntry/ContestEntry/Properties/Settings.Designer.cs
+++ ContestEntry/ContestEntry/Properties/Settings.Designer.cs
@@ -26,8 +26,8 @@ namespace ContestEntry.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\SampleDatabas" +
-            "e.mdf;Integrated Security=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Database\\Samp" +
+            "leDatabase.mdf;Integrated Security=True")]
         public string SampleDatabaseConnectionString {
             get {
                 return ((string)(this["SampleDatabaseConnectionString"]));

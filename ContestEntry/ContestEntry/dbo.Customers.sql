eec883b9d95f53f19bd65ff01afb24b0650c503a
meowFlute <mscottchristensen@gmail.com>
2016-04-20 20:52:54 -0600

    updated scripts for table generation

--- ContestEntry/ContestEntry/dbo.Customers.sql
+++ ContestEntry/ContestEntry/dbo.Customers.sql
@@ -1,7 +1,8 @@
-﻿CREATE TABLE [dbo].[Customers]
-(
-	[CustomerID] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
-    [FirstName] NVARCHAR(50) NOT NULL, 
-    [LastName] NVARCHAR(50) NOT NULL, 
-    [Email] NVARCHAR(200) NOT NULL
-)
+﻿CREATE TABLE [dbo].[Customers] (
+    [CustomerID] UNIQUEIDENTIFIER NOT NULL,
+    [FirstName]  NVARCHAR (50)    NOT NULL,
+    [LastName]   NVARCHAR (50)    NOT NULL,
+    [Email]      NVARCHAR (200)   NOT NULL,
+    PRIMARY KEY CLUSTERED ([CustomerID] ASC)
+);
+
---
+++ ContestEntry/ContestEntry/dbo.Orders.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Orders] (
+    [Id]          INT              IDENTITY (1, 1) NOT NULL,
+    [CustomerID]  UNIQUEIDENTIFIER NOT NULL,
+    [OrderDate]   DATETIME2 (7)    NOT NULL,
+    [OrderStatus] INT              NOT NULL,
+    [ItemsTotal]  DECIMAL (18, 4)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Orders_Customers] FOREIGN KEY ([CustomerID]) REFERENCES [dbo].[Customers] ([CustomerID])
+);
+
